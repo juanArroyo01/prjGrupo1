464124953d946b71570a3032320ebc30c65171fd
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-12 22:24:27 -0500

    Problem solved in App.java, anyone can use from 5 to the highest number

--- prjGrupo1/src/App.java
+++ prjGrupo1/src/App.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
+import juanarroyo.ForArroyo;
+import juanarroyo.ForJuan;
+import juanarroyo.ForJuanArroyo;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
+
+        ForJuan fj = new ForJuan();
+        ForArroyo fa = new ForArroyo();
+        ForJuanArroyo fja = new ForJuanArroyo();
 
         /*
          * ForJuan ja = new ForJuan();
@@ -22,23 +30,35 @@ public class App {
 
         System.out.println("\n--> Taller 1 <--\n");
 
-        do {
-            System.out.print("-> Ingrese un tamaño definido mayor a 3: ");
-            aux = keyboard.nextLine().charAt(0);
+        while (true) {
+            System.out.print("-> Ingrese un tamaño definido mayor a 4: ");
+            auxString = keyboard.nextLine();
 
-            if (((int) aux - '0') < 4) {
-                System.out.println("\nHa ingresado un bajo del limite, ingrese nuevamente...\n");
-            } else if (Character.isDigit(aux) && ((int) aux - '0') >= 4) {
-                size = aux - '0';
+            for (int i = 0; i < auxString.length(); i++) {
+                if (!Character.isDigit(auxString.charAt(i))) {
+                    isCorrect = false;
+                } else {
+                    isCorrect = true;
+                }
+            }
+
+            if (isCorrect) {
+                size = Integer.valueOf(auxString);
+                System.out.println(size);
+                if (size <= 4) {
+                    System.out.println("\nHa ingresado un bajo del limite, ingrese nuevamente...\n");
+                } else {
+                    break;
+                }
             } else {
                 System.out.println("\nHa ingresado un valor no numérico, ingrese nuevamente...\n");
                 size = -1;
             }
-        } while (size < 4);
+        }
 
-        do
+        isCorrect = false;
 
-        {
+        do {
             System.out.print("\n-> Ingrese un caracter: ");
             auxString = keyboard.nextLine();
             if (!auxString.isEmpty()) {
@@ -46,7 +66,7 @@ public class App {
                 isCorrect = true;
             } else {
                 System.out.println("\nHa ingresado un valor vacío, ingrese nuevamente...\n");
-                symb = '.';
+                symb = ' ';
                 isCorrect = false;
             }
         } while (!isCorrect);
@@ -58,7 +78,11 @@ public class App {
          * Y se imprimira su letra
          */
 
-        System.out.println("Size: " + size + ", Symb: " + symb);
+        fj.forNombreJ(size, symb);
+        System.out.println("\n");
+        fa.forApellidoA(size, symb);
+        System.out.println("\n");
+        fja.forNombreApellidoJA(size, symb);
 
         keyboard.close();
     }
