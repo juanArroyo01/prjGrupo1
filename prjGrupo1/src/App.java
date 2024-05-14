1c6daad18b091b9c07e7d57b918e599257b0d2ed
Anto-CasaT <150099532+Anto-CasaT@users.noreply.github.com>
2024-05-13 21:08:15 -0500

    Cambios pequeños

--- prjGrupo1/src/App.java
+++ prjGrupo1/src/App.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
-import juanarroyo.ForArroyo;
-import juanarroyo.ForJuan;
-import juanarroyo.ForJuanArroyo;
+import juanarroyo.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
