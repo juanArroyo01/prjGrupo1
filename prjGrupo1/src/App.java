9e72af77872f35bc4740c5781e1ee5ace6ed2b37
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-15 09:55:44 -0500

    Changes in main

--- prjGrupo1/src/App.java
+++ prjGrupo1/src/App.java
@@ -1,100 +1,52 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
-import juanarroyo.WhileArroyo;
-import juanarroyo.DoWhileArroyo;
-import juanarroyo.DoWhileJuan;
-import juanarroyo.DoWhileJuanArroyo;
-import juanarroyo.ForArroyo;
-import juanarroyo.ForJuan;
-import juanarroyo.ForJuanArroyo;
-import juanarroyo.WhileJuan;
-import juanarroyo.WhileJuanArroyo;
-
-import marlonchimarro.forChimarroC;
-import marlonchimarro.doWhileChimarroC;
-import marlonchimarro.doWhileMarlonM;
-import marlonchimarro.whileChimarroC;
-import marlonchimarro.whileMarlonM;
-import marlonchimarro.whileMarlonChimarroMC;
-import marlonchimarro.doWhileMarlonChimarroMC;
-import marlonchimarro.forMarlonM;
-import marlonchimarro.forMarlonChimarroMC;
-=======
-import JakeBenavides.DoWhileBenavides;
-import JakeBenavides.DoWhileJake;
-import JakeBenavides.DoWhileJakeBenavides;
-import JakeBenavides.ForBenavides;
-import JakeBenavides.ForJakeBenavides;
-import JakeBenavides.ForJakeJ;
-import JakeBenavides.WhileBenavides;
-import JakeBenavides.WhileJake;
-import JakeBenavides.WhileJakeBenavides;
+import marlonchimarro.*;
+import JakeBenavides.*;
 import juanarroyo.*;
->>>>>>> jakebenavides
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        
-        /*
-         * ForJuan fj = new ForJuan();
-         * ForArroyo fa = new ForArroyo();
-         * ForJuanArroyo fja = new ForJuanArroyo();
-         * WhileJuan wj = new WhileJuan();
-         * WhileArroyo wa = new WhileArroyo();
-         * WhileJuanArroyo wja = new WhileJuanArroyo();
-         * DoWhileJuan dwj = new DoWhileJuan();
-         * DoWhileArroyo dwa = new DoWhileArroyo();
-         * DoWhileJuanArroyo dwja = new DoWhileJuanArroyo();
-         */
 
+        ForJuan fj = new ForJuan();
+        ForArroyo fa = new ForArroyo();
+        ForJuanArroyo fja = new ForJuanArroyo();
+        WhileJuan wj = new WhileJuan();
+        WhileArroyo wa = new WhileArroyo();
+        WhileJuanArroyo wja = new WhileJuanArroyo();
+        DoWhileJuan dwj = new DoWhileJuan();
+        DoWhileArroyo dwa = new DoWhileArroyo();
+        DoWhileJuanArroyo dwja = new DoWhileJuanArroyo();
 
-        /*
-         * whileMarlonM wm = new whileMarlonM();
-         * whileChimarroC wc = new whileChimarroC();
-         * whileMarlonChimarroMC wmc = new whileMarlonChimarroMC();
-         * doWhileMarlonM dwm = new doWhileMarlonM();
-         * doWhileChimarroC dwc = new doWhileChimarroC();
-         * doWhileMarlonChimarroMC dwmc = new doWhileMarlonChimarroMC();
-         * forMarlonM fm = new forMarlonM();
-         * forChimarroC fc = new forChimarroC();
-         * forMarlonChimarroMC fmc = new forMarlonChimarroMC();
-         */
-       
-       /*  DoWhileCasaC dwcc= new DoWhileCasaC ();
+        whileMarlonM wm = new whileMarlonM();
+        whileChimarroC wc = new whileChimarroC();
+        whileMarlonChimarroMC wmc = new whileMarlonChimarroMC();
+        doWhileMarlonM dwm = new doWhileMarlonM();
+        doWhileChimarroC dwc = new doWhileChimarroC();
+        doWhileMarlonChimarroMC dwmc = new doWhileMarlonChimarroMC();
+        forMarlonM fm = new forMarlonM();
+        forChimarroC fc = new forChimarroC();
+        forMarlonChimarroMC fmc = new forMarlonChimarroMC();
+
+        DoWhileCasaC dwcc = new DoWhileCasaC();
         DoWhileStalinS dws = new DoWhileStalinS();
         DoWhileStalinCasaSC dwsc = new DoWhileStalinCasaSC();
-        WhileCasaC wcc= new WhileCasaC();
+        WhileCasaC wcc = new WhileCasaC();
         WhileStalinS ws = new WhileStalinS();
         WhileStalinCasaSC wsc = new WhileStalinCasaSC();
-        ForCasaC fcc= new ForCasaC();
-        ForStalinS fcs= new ForStalinS();
-        ForStalinCasaSC Fcsc = new ForStalinCasaSC();  
-        */   
-        
-         /*
-         * ForJuan ja = new ForJuan();
-         * 
-         * De esta forma se crea el objeto clase.
-         * 
-         * Para llamar al metodo de aquella clase es:
-         * ja.forNombre(parametro 1, parametro 2);
-         * 
-         * Y de esa forma ya se ejecuta el codigo ingresdo dentro de aquel metodo.
-         */
+        ForCasaC fcc = new ForCasaC();
+        ForStalinS fcs = new ForStalinS();
+        ForStalinCasaSC Fcsc = new ForStalinCasaSC();
 
-         /* 
-            ForJakeJ fjj =  new ForJakeJ();
-            ForBenavides fb = new ForBenavides();
-            ForJakeBenavides fbj = new ForJakeBenavides();
-            WhileJake wjj = new WhileJake();
-            WhileBenavides wb = new WhileBenavides();
-            WhileJakeBenavides wjb = new WhileJakeBenavides();
-            DoWhileJake dwjj = new DoWhileJake();
-            DoWhileBenavides dwb = new DoWhileBenavides();
-            DoWhileJakeBenavides dwjb = new DoWhileJakeBenavides();
-         */ 
+        ForJakeJ fjj = new ForJakeJ();
+        ForBenavides fb = new ForBenavides();
+        ForJakeBenavides fbj = new ForJakeBenavides();
+        WhileJake wjj = new WhileJake();
+        WhileBenavides wb = new WhileBenavides();
+        WhileJakeBenavides wjb = new WhileJakeBenavides();
+        DoWhileJake dwjj = new DoWhileJake();
+        DoWhileBenavides dwb = new DoWhileBenavides();
+        DoWhileJakeBenavides dwjb = new DoWhileJakeBenavides();
 
         char symb;
         String auxString;
@@ -143,77 +95,81 @@ public class App {
             }
         } while (!isCorrect);
 
-        /*
-         * Al crear su clase objeto y haber implementado, acontinuacion deben realizar:
-         * ja.forNombre(size, symb);
-         * 
-         * Y se imprimira su letra
-         */
+        System.out.println("For - Juan Arroyo");
+        fj.forNombreJ(size, symb);
+        System.out.println("\n");
+        fa.forApellidoA(size, symb);
+        System.out.println("\n");
+        fja.forNombreApellidoJA(size, symb);
+        System.out.println("\n\nWhile - Juan Arroyo");
+        wj.whileNombreJ(size, symb);
+        System.out.println("\n");
+        wa.whileApellidoA(size, symb);
+        System.out.println("\n");
+        wja.whileNombreApellidoJA(size, symb);
+        System.out.println("\n\nDo While - Juan Arroyo");
+        dwj.doWhileNombreJ(size, symb);
+        System.out.println("\n");
+        dwa.doWhileApellido(size, symb);
+        System.out.println("\n");
+        dwja.doWhileNombreApellidoJA(size, symb);
 
-        /*
-         * System.out.println("For - Juan Arroyo");
-         * fj.forNombreJ(size, symb);
-         * System.out.println("\n");
-         * fa.forApellidoA(size, symb);
-         * System.out.println("\n");
-         * fja.forNombreApellidoJA(size, symb);
-         * System.out.println("\n\nWhile - Juan Arroyo");
-         * wj.whileNombreJ(size, symb);
-         * System.out.println("\n");
-         * wa.whileApellidoA(size, symb);
-         * System.out.println("\n");
-         * wja.whileNombreApellidoJA(size, symb);
-         * System.out.println("\n\nDo While - Juan Arroyo");
-         * dwj.doWhileNombreJ(size, symb);
-         * System.out.println("\n");
-         * dwa.doWhileApellido(size, symb);
-         * System.out.println("\n");
-         * dwja.doWhileNombreApellidoJA(size, symb);
-         */
+        System.out.println("For - MarlonChimarroMC");
+        fm.formarlonm(size, symb);
+        System.out.println("\n");
+        fc.forchimarroc(size, symb);
+        System.out.println("\n");
+        fmc.formarlonchimarromc(size, symb);
+        System.out.println("\n\nWhile - MarlonChimarroMC");
+        wm.whilemarlonm(size, symb);
+        System.out.println("\n");
+        wc.whilechimarroc(size, symb);
+        System.out.println("\n");
+        wmc.whilemarlonchimarromc(size, symb);
+        System.out.println("Do While - MarlonChimarroMC");
+        dwm.dowhilemarlonm(size, symb);
+        System.out.println("\n");
+        dwc.dowhilechimarroc(size, symb);
+        System.out.println("\n");
+        dwmc.dowhilemarlonchimarromc(size, symb);
 
-         /*
-         * System.out.println("For - MarlonChimarroMC");
-         * fm.formarlonm(size, symb);
-         * System.out.println("\n");
-         * fc.forchimarroc(size, symb);
-         * System.out.println("\n");
-         * fmc.formarlonchimarromc(size, symb);
-         * System.out.println("\n\nWhile - MarlonChimarroMC");
-         * wm.whilemarlonm(size, symb);
-         * System.out.println("\n");
-         * wc.whilechimarroc(size, symb);
-         * System.out.println("\n");
-         * wmc.whilemarlonchimarromc(size, symb);
-         * System.out.println("Do While - MarlonChimarroMC");
-         * dwm.dowhilemarlonm(size, symb);
-         * System.out.println("\n");
-         * dwc.dowhilechimarroc(size, symb);
-         * System.out.println("\n");
-         * dwmc.dowhilemarlonchimarromc(size, symb); 
-         */
-       
-       /*   System.out.println("For-StalinCasaSC");
-        fcs.forNombreS(size,symb);
+        System.out.println("For-StalinCasaSC");
+        fcs.forNombreS(size, symb);
         System.out.println("\n");
         fcc.forApellidoC(size, symb);
         System.out.println("\n");
-        Fcsc.imprimirIniciales(size,symb);
+        Fcsc.imprimirIniciales(size, symb);
         System.out.println("\n\nWhile - StalinCasa");
         ws.whileNombreS(size, symb);
         System.out.println("\n");
         wcc.whileApellidoC(size, symb);
         System.out.println("\n");
-        wsc.whileNombreApellido(size,symb);
+        wsc.whileNombreApellido(size, symb);
         System.out.println("\n\nDo While - StalinCasa");
-        dws.imprimirDoWhileS(size,symb);
+        dws.imprimirDoWhileS(size, symb);
         System.out.println("\n");
-        dwcc.doWhileApellidoC (size,symb);
+        dwcc.doWhileApellidoC(size, symb);
         System.out.println("\n");
-        dwsc.doWhileNomApeliSC(size,symb);
-        */
+        dwsc.doWhileNomApeliSC(size, symb);
 
-        
-
+        System.out.println("For - Jake Benavides");
+        fjj.forNombreJ(size, symb);
+        System.out.println("\n");
+        fb.ForApellidoB(size, symb);
+        System.out.println("\n");
+        fbj.ForNombreApellidoJB(size, symb);
+        System.out.println("\n\nWhile - Jake Benavides");
+        wjj.WhileNombreJ(size, symb);
+        System.out.println("\n");
+        wb.WhileApellidoB(size, symb);
+        System.out.println("\n");
+        wjb.WhileNombreApellidoJB(size, symb);
+        System.out.println("\n\nDo While - Jake Benavides");
+        dwjj.DoWhileNombreJ(size, symb);
+        System.out.println("\n");
+        dwb.DoWhileApellidoB(size, symb);
+        System.out.println("\n");
+        dwjb.DoWhileNombreApellidoJB(size, symb);
 
         keyboard.close();
     }
