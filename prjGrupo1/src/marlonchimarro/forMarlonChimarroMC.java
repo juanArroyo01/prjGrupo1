7b831aed5c8a0be3c3c3ffff759d811302252ac5
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-15 16:19:12 -0500

    Class modified in marlonchimarro folder

--- prjGrupo1/src/marlonchimarro/forMarlonChimarroMC.java
+++ prjGrupo1/src/marlonchimarro/forMarlonChimarroMC.java
@@ -3,37 +3,28 @@ package marlonchimarro;
 public class forMarlonChimarroMC {
     public void formarlonchimarromc(int size, char symb) {
 
-        // Impresion de ambas letras
-        System.out.println("Impresion de letras:");
-        int i = 1;
-        while (i <= size) {
-            // Letra M
-            int j = 1;
-            while (j <= size) {
+        for (int i = 1; i <= size; i++) {
+            // letra M
+            for (int j = 1; j <= size; j++) {
                 if (j == 1 || j == size || (i + j == size + 1 && i <= size / 2 + 1) || (i == j && i <= size / 2)) {
                     System.out.print(symb + " ");
                 } else {
-                    System.out.print("  "); // Espacios
+                    System.out.print("  ");
                 }
-                j++;
             }
 
-            // Separadores
             System.out.print("  ");
 
             // Letra C
-            j = 1;
-            while (j <= size) {
+            for (int j = 1; j <= size; j++) {
                 if (i == 1 || i == size || j == 1) {
                     System.out.print(symb + " ");
                 } else {
-                    System.out.print("  "); 
+                    System.out.print("  ");
                 }
-                j++;
             }
 
-            System.out.println(); // Siguiente linea
-            i++;
+            System.out.println();
         }
 
     }
